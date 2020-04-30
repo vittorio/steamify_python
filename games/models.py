168edaebee000ca740cc20a470e9c6c4825e6bd3
Victor <sexsportandrocknroll@gmail.com>
2020-04-30 23:40:00 +0300

    advanced Game model

--- games/models.py
+++ games/models.py
@@ -1,12 +1,17 @@
 from django.db import models
 
-class Game(models.Model):
-    name = models.CharField(max_length=100, blank=True)
 
-    # appid = models.IntegerField()
-    # playtime_forever = models.IntegerField()
-    # img_icon_url = models.CharField(max_length=100, blank=True)
-    # img_logo_url = models.CharField(max_length=100, blank=True)
+class Game(models.Model):
+    appid = models.IntegerField(primary_key=True, editable=True)
+    price = models.IntegerField(default=0)
+
+    name = models.TextField()
+
+    playtime_forever = models.IntegerField(default=0)
+    playtime_two_weeks = models.IntegerField(default=0)
+
+    icon_url = models.TextField(blank=True)
+    logo_url = models.TextField(blank=True)
 
     def __str__(self):
         return f'{self.name}'
--- games/views.py
+++ games/views.py
@@ -4,23 +4,34 @@ from steam import WebAPI
 from .models import Game
 from .serializers import GameSerializer
 
+# TODO move variable to env
 api = WebAPI(key='1AD897533C698E617B4F351C640EC53E')
 res = api.IPlayerService.GetOwnedGames(
     key='1AD897533C698E617B4F351C640EC53E',
     steamid='76561198080321262',
     include_appinfo=True,
+    include_free_sub=False,
     include_played_free_games=True,
     appids_filter=[])
 
 games = res.get('response').get('games')
 
 for game in games:
-    if game.get('playtime_two_weeks'):
-        print(game)
-# Game.objects.update_or_create(name=game.get('name'), defaults={"name": game.get('name')})
-#     serializer = GameSerializer(data=game)
-#     if serializer.is_valid():
-#         serializer.save()
+    print(game)
+
+    appid = game.get('appid')
+    defaults = {
+        'name': game.get('name'),
+        'appid': appid,
+        'playtime_forever': game.get('playtime_forever'),
+        'icon_url': f'http://media.steampowered.com/steamcommunity/public/images/apps/{appid}/{game.get("img_icon_url")}.jpg',
+        'logo_url': f'http://media.steampowered.com/steamcommunity/public/images/apps/{appid}/{game.get("img_logo_url")}.jpg'
+    }
+
+    if game.get('playtime_2weeks'):
+        defaults['playtime_two_weeks'] = game.get('playtime_2weeks')
+
+    Game.objects.update_or_create(pk=appid, defaults=defaults)
 
 
 class GameViewSet(viewsets.ModelViewSet):
